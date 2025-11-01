2b8301615826c794fbf4c188b3faa84ca98a3a3d
antlerjely96 <antlerjely96@gmail.com>
2025-11-01 14:54:29 +0700

    demo CSharp buổi 1

---
+++ Buoi1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World");
+/*
+	2. Nhap 1 so nguyen.
+	Kiem tra so nguyen do la so chan hay le
+	Goi ý: a % 2 == 0
+	
+	3. Nhap so tuoi cua 1 nguoi tu ban phim (tuoi la so nguyen)
+	Kiem tra:
+	Neu tuoi < 0: nhạp sai
+	Neu tuoi < 6: hoc mau giao
+	Neu tuoi < 11: hoc cap 1
+	Neu tuoi < 16: hoc cap 2
+	Neu tuoi < 18: hoc cap 3
+	Neu tuoi < 23: Hoc dai hoc
+	Neu tuoi <= 65: Di lam
+	Con lai: Nghi huu
+	
+	4. Nhap diem trung binh cua 1 hoc sinh.
+	Nếu diem < 0 || diem > 10 => Nhap sai
+	Neu diem < 5 => Yeu
+	Neu diem < 6.5 => trung binh
+	Neu && diem < 8 => Kha
+	Con lai => gioi
+	
+	5. Nhap thang la 1 so nguyen. Hien thi so ngay trong thang do
+	
+	6. Nhap 3 so nguyen a, b, c.
+	Hien thi so lon nhat va so nho nhat trong 3 so
+	
+	7. Nhap 3 so thuc a, b, c.
+	Kiem tra 3 so do co tao thanh tam giac khong.
+	
+	8. Hien thi menu sau:
+	1. Nhap 2 canh cua hinh chu nhat
+	Tinh chu vi, dien tich hinh chu nhat
+	2. Nhap canh cua hinh vuong.
+	Tinh chu vi, dien tich hinh vuong
+	3. Nhap ban kinh cua hinh tron
+	Tinh chu vi, dien tich hinh tron
+	Chon:
+	Khi nhap so tu 1 den 3 thi se thuc hien menu tuong ung
+*/
